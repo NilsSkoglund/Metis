2ac9f0bebba69c3e209e6096ae6d8d019467162d
puranchen <87152497+puranchen@users.noreply.github.com>
2023-12-07 15:07:44 +0100

    reverted back to default age calculation

--- backend/patients/models.py
+++ backend/patients/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from datetime import date
 from dateutil.relativedelta import relativedelta
 
-
 class Patient(models.Model):
     first_name = models.CharField(max_length=100) # Minimum requirement for creating a patient object
     middle_name = models.CharField(max_length=100, null=True, blank=True)
@@ -10,6 +9,7 @@ class Patient(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     _age = models.IntegerField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
     @property
     def age(self):
@@ -21,12 +21,8 @@ class Patient(models.Model):
                     (today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day))
         elif self._age is not None:
             # Age calculation based on created_at and manually set _age
-            # Implementation allows for incrementation of age over time
-            # TODO: Should I indicate if the age is "EXACT" or "Approximated" based on created_at
-            reference_date = self.created_at.date() + relativedelta(years=self._age)
-            today = date.today()
-            return today.year - reference_date.year - (
-                    (today.month, today.day) < (reference_date.month, reference_date.day))
+            # TODO: Implementation allows for incrementation of age over time
+            return self._age
         return self._age
 
     @age.setter
@@ -44,8 +40,11 @@ class Patient(models.Model):
     gender = models.CharField(max_length=1, null=True, choices=GENDER_CHOICES)
 
     def save(self, *args, **kwargs):
+        if self.date_of_birth is None and self._age is not None:
+            # If _age is provided but date_of_birth is not, set age directly
+            self.age = self._age
         # Calculate age from date_of_birth if it's available and if age isn't explicitly set
-        if self.date_of_birth:
+        elif self.date_of_birth:
             today = date.today()
             calculated_age = today.year - self.date_of_birth.year - (
                 (today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day))
