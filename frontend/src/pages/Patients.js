7b82721ce6648ca0545fe0e543bd054c69ac8449
puranchen <87152497+puranchen@users.noreply.github.com>
2023-12-07 15:11:17 +0100

    New PatientCard, delete-request and refactoring of fetchPatients

--- frontend/src/pages/Patients.js
+++ frontend/src/pages/Patients.js
@@ -3,40 +3,60 @@ import Box from "@mui/material/Box";
 import NewPatientModal from "../components/NewPatientModal";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {Typography} from "@mui/material";
+import {Card, CardActions, CardContent, Typography} from "@mui/material";
+import PatientCard from "../components/PatientCard";
 
 export default function Patients() {
 
     const [patients, setPatients] = useState([]);
-
     const [openModal, setOpenModal] = useState(false)
-    const handleCloseModal = () => setOpenModal(false)
-    const handleOpenModal = () => setOpenModal(true)
-
-    useEffect(() => {
-        const fetchPatients = async () => {
+    const fetchPatients = async () => {
             try {
                 const response = await axios.get('http://127.0.0.1:8000/patients/');
                 setPatients(response.data);
+                return true;
             } catch (error) {
                 console.error('Error fetching patients:', error);
+                return false;
             }
         };
 
-        fetchPatients();
+    useEffect(() => {
+        fetchPatients()
     }, []);
+
+    const handleCloseModal = async () => {
+        setOpenModal(false);
+        await fetchPatients()
+    }
+    const handleOpenModal = () => setOpenModal(true)
+
+    const handleRemove = async (patientId) => {
+        try {
+            await axios.delete(`http://127.0.0.1:8000/patients/${patientId}`)
+            setPatients(patients.filter(patient => patient.id !== patientId))
+        } catch (error) {
+            console.error('Error removing patient:', error);
+        }
+    }
 
     return (
         <React.Fragment>
             <Box display="flex" alignItems={'center'} justifyContent={'center'} marginBottom={3}>
-                <Button variant="contained" onClick={handleOpenModal}>Add New Patient</Button>
+                <Button variant="outlined" color='success' onClick={handleOpenModal}>Add New Patient</Button>
             </Box>
             <NewPatientModal open={openModal} onClose={handleCloseModal}/>
-            {patients.map((patient) => (
-                <Typography>
-                    {patient.first_name}
-                </Typography>
-            ))}
+            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center' }}>
+                {patients.map((patient) => (
+
+                    <PatientCard
+                        key={patient.id}
+                        patient={patient}
+                        onRemove={handleRemove}
+                    />
+
+                ))}
+            </div>
 
         </React.Fragment>
     )
