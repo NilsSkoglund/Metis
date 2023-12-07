b00f126e7968c7fa2c75269bdc54d845044fb466
puranchen <87152497+puranchen@users.noreply.github.com>
2023-12-07 15:08:02 +0100

    serialized age-calculation

--- backend/patients/serializers.py
+++ backend/patients/serializers.py
@@ -3,6 +3,9 @@ from .models import Patient
 
 
 class PatientSerializer(serializers.ModelSerializer):
+    age = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = Patient
-        fields = '__all__'
+        fields = ["id", "first_name", "middle_name", "last_name", "age", "_age", "gender", "created_at", "modified_at",
+                  "date_of_birth"]
